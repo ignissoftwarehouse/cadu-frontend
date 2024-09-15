a733fc8eb2d539352c0e60966f102da1974967b0
Veronica Toledo <vntoledo30@gmail.com>
2024-09-15 10:54:56 -0300

    Ajustes de scss

---
+++ cadu-frontend/src/app/shared/components/action-button/action-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-action-button',
+  templateUrl: './action-button.component.html',
+  styleUrls: ['./action-button.component.scss']
+})
+export class ActionButtonComponent {
+  @Input() public src!: string;
+  @Input() public alt!: string;
+}
---
+++ cadu-frontend/src/app/shared/components/divider/divider/divider.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divider',
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.scss']
+})
+export class DividerComponent { }
